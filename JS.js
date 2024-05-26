b1341fabf29b19eaac2746c3462f469b55b33c43
MELCM1234 <163954413+MELCM1234@users.noreply.github.com>
2024-05-26 17:25:15 -0400

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menu = document.querySelector(".menu");
+
+  menuBtn.addEventListener("click", function() {
+      menu.classList.toggle("show");
+  });
+});
+
+
+
+
+
